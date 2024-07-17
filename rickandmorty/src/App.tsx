62ffc14abf7f0d8b5d5b3794fedc4b1a470ff6bd
williamsdantas <williamsad9@gmail.com>
2024-07-17 17:19:14 -0300

    atualização do navbar, sem rotas ainda

--- rickandmorty/src/App.tsx
+++ rickandmorty/src/App.tsx
@@ -1,34 +1,32 @@
-import { Navbar, Container, Nav } from "react-bootstrap"
-import logo from './assets/logo.png'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from "react-router-dom"
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "./assets/logo.png";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import { Link } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
       <div id="_header" className="divHeader">
-        <div id="interna" className='divTitulo'>
+        <div id="interna" className="divTitulo">
           <div id="logo" className="logo">
             <img src={logo} alt="logo"></img>
           </div>
           <div>
-            <p className='titulo'>The Rick and Morty APP</p>
-            <Navbar expand="lg" className="bg-body-tertiary">
-              <Container>
-                <Navbar.Brand href="#home">R & M</Navbar.Brand>
+            <p className="titulo"> The Rick and Morty APP</p>
+            <Navbar expand="lg" className="bg-body-tertiary" >
+              <Container >
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="me-auto">
-                    <Nav.Link><Link to='/'>Home</Link></Nav.Link>
-                    <Nav.Link>
-                      <Link to='/personagens'>Personagens</Link>
-                    </Nav.Link>
-                    <Nav.Link>
-                      <Link to='/episodios'>Episódios</Link>
-                    </Nav.Link>
+                  <Nav className="me-auto menu">
+                    <Nav.Link href="#personagens">Personagens</Nav.Link>
+                    <Nav.Link href="#episodios">Episódios</Nav.Link>                    
+                    <Nav.Link href="#dimensoens">Dimensões</Nav.Link>
+                    <Nav.Link href="#sobre">Sobre</Nav.Link> 
                   </Nav>
                 </Navbar.Collapse>
               </Container>
@@ -36,9 +34,9 @@ function App() {
           </div>
         </div>
       </div>
-
+      <div></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
