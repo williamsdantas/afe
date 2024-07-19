acd43bfb691ee5ed1286b7f26a3f052ed5735f35
williamsdantas <williamsad9@gmail.com>
2024-07-19 16:54:57 -0300

    atualização :(

--- rickandmorty/src/App.tsx
+++ rickandmorty/src/App.tsx
@@ -1,40 +1,18 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "./assets/logo.png";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
-import { Link } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/navbar/navbar";
+
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div id="_header" className="divHeader">
-        <div id="interna" className="divTitulo">
-          <div id="logo" className="logo">
-            <img src={logo} alt="logo"></img>
-          </div>
-          <div>
-            <p className="titulo"> The Rick and Morty APP</p>
-            <Navbar expand="lg" className="bg-body-tertiary" >
-              <Container >
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="ms-auto">
-                    <Nav.Link href="#personagens">Personagens</Nav.Link>
-                    <Nav.Link href="#episodios">Episódios</Nav.Link>                    
-                    <Nav.Link href="#dimensoens">Dimensões</Nav.Link>
-                    <Nav.Link href="#sobre">Sobre</Nav.Link> 
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
-          </div>
-        </div>
+      <div >
+        <NavBar/>
       </div>
-      <div></div>
+
+      <div className="content"> conteudo</div>
     </>
   );
 }
